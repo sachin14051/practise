259bac9a17d5f32582b98873a6ebc668c442f308
sachin <sachin14051@gmail.com>
2023-07-18 00:57:19 +0530

    print table 8

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 let cl = console.log;
 
-
-var a = 0 ;
-
-while(a<=100){
-    cl(a)
-    a++
+// print table 8
+for(let i= 1; i<=10; i++){
+      let s = i ;
+      let t = 8;
+      let u = s * t
+      cl(u)
 }
+
